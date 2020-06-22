b9eab9f96a83dd61c540a1088038bc4df31de6b4
William Ku <kucheech@gmail.com>
2020-06-23 04:02:24 +0800

    * change base url again

--- data/constants.js
+++ data/constants.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const JASMINE_TIMEOUT = 100000;
-const BASE_URL = 'https://d21prpgfce4oa9.cloudfront.net';
+// const BASE_URL = 'https://d21prpgfce4oa9.cloudfront.net';
+const BASE_URL = 'https://kuch.tk';
 
 module.exports = {
     JASMINE_TIMEOUT,
